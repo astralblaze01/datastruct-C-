c8655b32f6612e34b972576a386b552129d27bf0
astralblaze01 <ht0933@naver.com>
2025-03-17 23:41:42 +0900

    부분 집합 구현하는 것만 하면 됌

--- 자구과제(풀이)/04수업/04 추가 문제/01.c
+++ 자구과제(풀이)/04수업/04 추가 문제/01.c
@@ -1,12 +1,70 @@
+// 아직 구현 못함
+
 #include <stdio.h>
+#include <stdlib.h>
 
 FILE *initFile(const char *fileName, const char *type);
 int isOpen(FILE *f);
+int fileSize(FILE *f);
+int *initArray(FILE *f, int aSize);
+int *initCheckArr(int *arr, int size);
 
 int main()
 {
     FILE *fp = initFile("in.txt", "r");
+    int size = fileSize(fp);
+    int *check = (int *)malloc(sizeof(int) * size);
+    int *setArr = (int *)malloc(sizeof(int) * size);
 
+    setArr = initArray(fp, size);
+    check = initCheckArr(check, size);
+}
+
+// check 배열 초기화
+int *initCheckArr(int *arr, int size)
+{
+    while (size)
+    {
+        arr[--size] = 0;
+    }
+    return arr;
+}
+
+// 배열 초기화화
+int *initArray(FILE *f, int aSize)
+{
+    int *initArr = (int *)malloc(sizeof(int) * aSize);
+
+    if (!initArr)
+    {
+        printf("memory allocation is fail");
+        exit(1);
+    }
+
+    for (int i = 0; i < aSize; i++)
+    {
+        fscanf(f, "%d", &initArr[i]);
+    }
+
+    fseek(f, 0, SEEK_SET);
+
+    return initArr;
+}
+
+// 파일 크기 나타내는 함수
+int fileSize(FILE *f)
+{
+    int fVal, fSize = 0;
+
+    while (!feof(f)) // feof : 값이 있으면 0 없으면 1, 마지막에 도달했으면 1
+    {
+        fscanf(f, "%d", &fVal);
+        fSize++;
+    }
+
+    fseek(f, 0, SEEK_SET);
+
+    return fSize;
 }
 
 // 열리면 1 없으면 0
